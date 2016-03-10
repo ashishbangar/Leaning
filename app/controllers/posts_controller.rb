df3846e1d03da01b17f403e366c801f18becb0d6
ashish231992 <ashish231992@gmail.com>
2016-03-10 17:04:03 +0530

    Updated Code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
   
   def new  
     @post = Post.new
-
   end
 
   def create
@@ -14,16 +13,38 @@ class PostsController < ApplicationController
        render 'new'
     end
   end
+
   def show
     @post = Post.find(params[:id])
-    
+    redirect_to "http://localhost:3000/posts"
   end
-
+ 
   def index
-    @posts = Post.all
-    
+    @posts = Post.all    
   end
 
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+
+  def update
+  @post = Post.find(params[:id])
+ 
+    if @post.update(post_param)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+ 
+   redirect_to posts_path
+  end
+    
   private 
     def post_param
       params.require(:post).permit(:title, :text)
